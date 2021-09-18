58b929a4ec84c97ebc144af8d4f7d20175b68452
Michael Johnson <mjohnson@seso.global>
2021-09-18 22:01:11 +0000

    --worked on the routes

---
+++ src/Home.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const Home : FC = () =>{
+    return <div>Hello World! Home </div>;
+};
+
+export default Home;
